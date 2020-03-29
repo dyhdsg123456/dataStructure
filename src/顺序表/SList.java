50b3a82620b2f2dd63755690b8f0172d03cb2afa
duyuanhao <13775516233@163.com>
2020-03-29 14:59:53 +0800

    Initial commit

---
+++ src/顺序表/SList.java
@@ -0,0 +1,18 @@
+package 顺序表;
+
+/**
+ * Auther: dyh
+ * Date: 2020/3/28 21:34
+ * Description:
+ */
+public interface SList {
+     void clear();
+ void isEmpty();
+   void initSlist();
+
+      Object get(int i);
+    int getindex(Object o);
+    void insert(int i,Object o);
+   void remove (int i );
+
+}
---
+++ src/顺序表/SequenceList.java
@@ -0,0 +1,46 @@
+package 顺序表;
+
+/**
+ * Auther: dyh
+ * Date: 2020/3/28 21:26
+ * Description:
+ */
+public class SequenceList implements SList {
+    private int length;
+    private int[] data
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void isEmpty() {
+
+    }
+
+    @Override
+    public void initSlist() {
+
+    }
+
+    @Override
+    public Object get(int i) {
+        return null;
+    }
+
+    @Override
+    public int getindex(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void insert(int i, Object o) {
+
+    }
+
+    @Override
+    public void remove(int i) {
+
+    }
+}
