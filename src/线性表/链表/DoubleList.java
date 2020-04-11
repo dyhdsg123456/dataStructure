e6da1617548692de77c197077d49af95a17574f1
duyuanhao <13775516233@163.com>
2020-04-11 13:26:08 +0800

    双向链表增删操作

--- src/线性表/链表/DoubleList.java
+++ src/线性表/链表/DoubleList.java
@@ -25,16 +25,67 @@ public class DoubleList {
         }
 
     }
+    public void  remove(int index){
+        check(index);
+        Node node = getNode(index);
+        Node prev = node.prev;
+        Node next = node.next;
+        if(prev==null){
+            first=next;
+        }else {
+            prev.next=next;
+            node.prev=null;
+        }
+
+        if(next==null){
+            last=prev;
+        }else {
+            next.prev=prev;
+            node.next=null;
+        }
+        node.data=null;
+        size--;
+
+
+
+    }
 
     public void  add(int index,Object data ){
         check(index);
         Node node = getNode(index);
         //尾部插入
         if(index==size){
-
+            addlast(data);
         }else {
-
+            linkBefore(data,node);
         }
+    }
+
+    //在index位置的node之前插入
+    private void linkBefore(Object data, Node node) {
+        Node prev = node.prev;
+        Node newnode = new Node(prev, data, node);
+        node.prev=newnode;
+        if(prev==null){
+            first=newnode;
+        }else {
+            prev.next=newnode;
+        }
+        size++;
+    }
+
+    private void addlast(Object data) {
+        Node l = this.last;
+        //将新节点连到last后面
+        Node newnode = new Node(l,data,null);
+        if(l==null){
+            //链表为空
+            first=newnode;
+        }else {
+           l.next= newnode;
+        }
+        last=newnode;
+        size++;
     }
 
     //根据位置获取node
