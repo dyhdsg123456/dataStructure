e1ce34027cdaa54ff6760f8e74cba228c0f72f2c
duyuanhao <13775516233@163.com>
2020-06-05 22:15:38 +0800

    二分查找递归

--- src/查找/二分查找.java
+++ src/查找/二分查找.java
@@ -11,8 +11,12 @@ public class 二分查找 {
             int  k=3;
         int search = search(arr, k);
         System.out.println(search);
+        int low=0;
+        int high=arr.length-1;
+        int i = search2(arr, k, low, high);
+        System.out.println(i);
     }
-
+    //非递归
     public static int search(int[] arr,int k){
         int low=0; int high=arr.length-1;
         while (low<=high){
@@ -26,5 +30,20 @@ public class 二分查找 {
             }
         }
         return 0;
+    }
+    //递归
+    public static int search2(int[] arr,int k,int low,int high){
+       if(low>high){
+           return 0;
+       }
+       int mid=(high+low)/2;
+       if(k==arr[mid]){
+           return mid;
+       }else if(arr[mid]<k){
+           return search2(arr,k,mid+1,high);
+       }else {
+           return search2(arr,k,low,mid-1);
+       }
+
     }
 }
