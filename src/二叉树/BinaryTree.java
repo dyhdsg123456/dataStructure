e0b48f88bd84f922bb35a366a809acae9cf7cebf
duyuanhao <13775516233@163.com>
2020-06-14 21:00:36 +0800

    生成二叉排序树

--- src/二叉树/BinaryTree.java
+++ src/二叉树/BinaryTree.java
@@ -10,21 +10,28 @@ import java.util.Stack;
  */
 public class BinaryTree<T> {
     public static void main(String[] args) {
-        BinaryTree<Integer> right = new BinaryTree<Integer>(3,null,null);
-        BinaryTree<Integer> left = new BinaryTree<Integer>(4,null,null);
-        BinaryTree<Integer> tBinaryTree =  new BinaryTree<Integer>(1, left, right);
-        BinaryTree<Integer> tree =  new BinaryTree<Integer>(4, tBinaryTree, right);
+//        BinaryTree<Integer> right = new BinaryTree<Integer>(3,null,null);
+//        BinaryTree<Integer> left = new BinaryTree<Integer>(4,null,null);
+//        BinaryTree<Integer> tBinaryTree =  new BinaryTree<Integer>(1, left, right);
+//        BinaryTree<Integer> tree =  new BinaryTree<Integer>(4, tBinaryTree, right);
 //        tree.InOrderTraverse2(tree);
 //        System.out.println("==========");
 //        tree.InOrderTraverse4(tree);
-        BinaryTree<Object> tree1 = new BinaryTree<>();
-        String[] data={"A","B","C","#","#","D","E","#","G","#","#","F","#","#","#"};
-        create(data,tree1,0);
-       InOrderTraverse(tree1);
-        System.out.println(TreeDepth(tree1));
+//        BinaryTree<Object> tree1 = new BinaryTree<>();
+//        String[] data={"A","B","C","#","#","D","E","#","G","#","#","F","#","#","#"};
+//        create(data,tree1,0);
+//       InOrderTraverse(tree1);
+//        System.out.println(TreeDepth(tree1));
 //        BinaryTree<Integer> tree2 =  new BinaryTree<Integer>(1, right, null);
 //        System.out.println(TreeDepth(tree2));
-        System.out.println(TreeCount(tree1));
+//        System.out.println(TreeCount(tree1));
+        int[] ints = {4,5,7,10,9,6,8,1,2,3};
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        for (int anInt : ints) {
+            initTree(anInt,tree);
+        }
+        System.out.println(true);
+        InOrderTraverse2(tree);
     }
     T data;
     BinaryTree<T>  left;
@@ -35,6 +42,25 @@ public class BinaryTree<T> {
         this.data = data;
         this.left = left;
         this.right = right;
+    }
+    public static BinaryTree initTree(int data,BinaryTree<Integer> atree){
+        BinaryTree<Integer> tree = new BinaryTree<>(data,null,null);
+        if(atree.data==null){
+            atree.data=data;
+        }else if(data>atree.data){
+            if(atree.right==null){
+                atree.right=tree;
+            }else {
+                initTree(data,atree.right);
+            }
+        }else {
+            if(atree.left==null){
+                atree.left=tree;
+            }else {
+                initTree(data,atree.left);
+            }
+        }
+return atree;
     }
     //先序遍历
     public static void InOrderTraverse(BinaryTree tree){
@@ -54,7 +80,7 @@ public class BinaryTree<T> {
              //中序遍历左子树
             InOrderTraverse2(tree.left);
             //访问根节点
-            System.out.print(tree.data);
+            System.out.println(tree.data);
             //中序遍历右子树
             InOrderTraverse2(tree.right);
         }
